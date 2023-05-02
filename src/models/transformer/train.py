3f0704308fb2b8c6b42c9f0d08da0266153e9db5
krichard98 <kiss.richard@mkb.hu>
2023-05-02 14:50:44 +0200

    .

--- src/models/transformer/train.py
+++ src/models/transformer/train.py
@@ -35,42 +35,33 @@ class MaskedLanguageModel(LightningModule):
         inputs = inputs.transpose(0,1)
         targets = inputs.contiguous()[1:]
         inputs = inputs[:-1]
-        #print("TARGE", targets, "\nINPUT", inputs[:-1])
-
+        
         seq_len = inputs.shape[0]
         src_mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool()
-        #bs = batch.shape[0]
-        #src_key_padding_mask = torch.zeros(bs, seq_len).bool()#(inputs == self.pad_token_id).T.bool()
         src_key_padding_mask = (inputs == self.pad_token_id).T.bool()
-
-        #print("INPUT", inputs)
-        #print("TARGET", targets)
-        #print("SRC_MASK", src_mask)
-        #print("PAD_TOKEN", self.pad_token_id)
-        #print("PADDING_MASK", src_key_padding_mask)
-
+        
         predictions = self(inputs, src_mask=src_mask, src_key_padding_mask=src_key_padding_mask)
 
         loss = F.cross_entropy(predictions.view(-1, self.model.ntoken), targets.view(-1), ignore_index=self.pad_token_id)
-        #print(predictions[0,0], targets[0,0])
         self.log('train_loss', loss, prog_bar=True, on_step=True)
-        #import time
-        #time.sleep(.1)
         return loss
 
     def validation_step(self, batch, batch_idx):
-        inputs = batch
+        inputs = self._filter_out_too_shorts(batch)
+        assert inputs.shape[0] > 0, "Wrong batch (no sequence with more than 1 valid items) check _filter_out_too_shorts"
+        print(inputs.shape[0])
         inputs = inputs.transpose(0,1)
         targets = inputs.contiguous()[1:]
-
+        inputs = inputs[:-1]
+        
         seq_len = inputs.shape[0]
         src_mask = torch.triu(torch.ones(seq_len, seq_len), diagonal=1).bool()
-        src_key_padding_mask = (inputs == self.mask_token_id).T.bool()
-
+        src_key_padding_mask = (inputs == self.pad_token_id).T.bool()
+        
         predictions = self(inputs, src_mask=src_mask, src_key_padding_mask=src_key_padding_mask)
 
-        loss = F.cross_entropy(predictions[:-1].view(-1, self.model.ntoken), targets.view(-1), ignore_index=-100)
-        self.log('val_loss', loss, prog_bar=True)
+        loss = F.cross_entropy(predictions.view(-1, self.model.ntoken), targets.view(-1), ignore_index=self.pad_token_id)
+        self.log('val_loss', loss, prog_bar=True, on_step=True)
 
 
     def configure_optimizers(self):
