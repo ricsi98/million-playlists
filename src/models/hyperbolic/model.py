9208734909664a67c1c1403f34fd205e50ff1346
krichard98 <kiss.richard@mkb.hu>
2023-05-26 19:15:34 +0200

    .

--- src/models/hyperbolic/model.py
+++ src/models/hyperbolic/model.py
@@ -1,5 +1,6 @@
 import geoopt
 import layers
+import losses
 import lightning as pl
 
 
@@ -14,13 +15,18 @@ class ManifoldSkipGram(pl.LightningModule):
         elif similarity == "distance":
             self.sim = layers.ManifoldDistance(manifold)
         self.opt_kwargs = opt_kwargs
+        self.loss_fn = losses.SGNSLoss()
 
     def forward(self, a, b):
         va, vb = self.encoder(a), self.encoder(b)
         return self.sim(va, vb)
 
     def training_step(self, batch, batch_idx):
-        ...
+        x1, x2, y = batch
+        y_ = self(x1, x2)
+        loss = self.loss_fn(y_, y)
+        self.log("train_loss", loss.item())
+        return loss
 
     def configure_optimizers(self):
         return geoopt.optim.RiemannianSGD(self.parameters(), **self.opt_kwargs)
