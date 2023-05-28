0ec0f3872a8e632ca0032725756c1fb84a5a87e4
krichard98 <kiss.richard@mkb.hu>
2023-05-28 17:52:28 +0200

    .

--- src/data/skipgram/convert.py
+++ src/data/skipgram/convert.py
@@ -8,14 +8,40 @@ from ..utils import PlaylistIterator
 from . import transform as T
 
 
+DATASET_NAME = "data"
+
+
 class BufferedH5Writer:
 
-    def __init__(self, file, dataset) -> None:
+    def __init__(self, file, dataset, buffer_size) -> None:
         self.file = file
         self.dataset = dataset
+        self.buffer_size = buffer_size
+        self.buffer = []
+        self.index = 0
+        
+    def _len(self):
+        if len(self.buffer) == 0:
+            return 0
+        return sum(len(xi) for xi in self.buffer)
+        
+    def _write(self):
+        if self._len() == 0:
+            return
+        h, w = self.file[self.dataset].shape
+        n = self._len()
+        if n > h - self.index:
+            self.file[self.dataset].resize((self.index + n, w))
+        data = np.concatenate((self.buffer), axis=0).reshape(n, w)
+        self.file[self.dataset][self.index:self.index+n,:] = data
+        self.index += n
+        self.file.flush()
+        self.buffer = []
 
     def append(self, data: np.ndarray):
-        pass
+        self.buffer.append(data)
+        if self._len() >= self.buffer_size:
+            self._write()
 
 
 def convert(save_path: str, playlists: PlaylistIterator, k: int = 0, dtype=np.int64):
@@ -43,8 +69,9 @@ def convert(save_path: str, playlists: PlaylistIterator, k: int = 0, dtype=np.in
         T.TrackURI2Idx(song2idx)
     )
     sg = T.SkipGram(3)
-    with h5py.File(os.path.join(save_path, "skipgrams.h5")) as f:
-        dset = f.create_dataset()
+    with h5py.File(os.path.join(save_path, "skipgrams.h5"), "w") as f:
+        f.create_dataset(DATASET_NAME, (200, 2), maxshape=(None, 2))
+        bw = BufferedH5Writer(f, DATASET_NAME, 50000)
         for pl in playlists:
             pl = transform(pl)
             if len(pl) == 0: continue
@@ -53,13 +80,13 @@ def convert(save_path: str, playlists: PlaylistIterator, k: int = 0, dtype=np.in
             rhs = reduce(list.__add__, sgrams[1])
             lhs, rhs = np.array(lhs, dtype=dtype), np.array(rhs, dtype=dtype)
             data = np.stack((lhs, rhs), axis=1)
-            print(data.shape)
-            asd
+            bw.append(data)
+        bw._write()
 
 import os
 base_path = "./data/processed"
 files = os.listdir(base_path)
-files = [os.path.join(base_path, f) for f in files][:1]
+files = [os.path.join(base_path, f) for f in files]
 print(files)
 pl = PlaylistIterator(files)
-print(convert("./data/kcore", pl, 1000))
+print(convert("./data/kcore", pl, 30))
---
+++ src/data/skipgram/loader.py
@@ -0,0 +1,58 @@
+import os
+import json
+import h5py
+import torch
+import random
+import logging
+import numpy as np
+from torch.utils.data import Dataset
+
+
+class H5Dataset(Dataset):
+
+    DEFAULT_BUFFER_SIZE = 50_000_000
+
+    def __init__(self, path: str, alpha: float = 0.75, n_negatives: int = 1, buffer_size: int = DEFAULT_BUFFER_SIZE) -> None:
+        self.path = path
+        self.n_negatives = n_negatives
+        self.buffer_size = buffer_size
+        self._init_negative_distribution(path, alpha)
+        self.__data = {
+            "chunk_index": -1,
+            "data": torch.zeros((buffer_size, 2))
+        }
+        
+    def _init_negative_distribution(self, path: str, alpha: float):
+        with open(os.path.join(path, "frequencies.json"), "r") as f:
+            freqs = json.load(f)
+            weights = np.array(list(freqs.values())) ** alpha
+            weights /= np.sum(weights)
+            self.cum_weights = np.cumsum(weights)
+            self.ids = np.arange(len(weights), dtype=np.int64)
+
+    def _get(self, index):
+        chunk_index = index // self.buffer_size
+        if chunk_index != self.__data["chunk_index"]:
+            logging.info(f"Loading chunk {chunk_index}")
+            path = os.path.join(self.path, "skipgrams.json")
+            with h5py.File(path, "r") as f:
+                start_ = chunk_index * self.buffer_size 
+                end_ = (chunk_index + 1) * self.buffer_size
+                assert start_ < len(f["data"]), "index out of bounds"
+                self.__data["data"][:] = f["data"][start_:end_]
+                self.__data["chunk_index"] = chunk_index
+        offset = index - chunk_index * self.buffer_size
+        return self.__data["data"][offset]
+
+    def __len__(self):
+        if not hasattr(self, "__h5_size"):
+            path = os.path.join(self.path, "skipgrams.json")
+            with h5py.File(path, "r") as f:
+                self.__h5_size = len(f["data"])
+        return self.__h5_size
+
+    def _negatives(self, k):
+        return np.array(random.choices(self.ids, cum_weights=self.cum_weights, k=k))
+
+    def __getitem__(self, index):
+        return self._get(index)
